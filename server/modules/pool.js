cc0897a880a2157547c9b231e99e14885d7e6640
Kimberly Orchard <kimberly.a.orchard@gmail.com>
2020-11-23 11:40:09 -0600

    updated pool config for heroku

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,13 +1,26 @@
 const pg = require('pg'); // requiring in database
 
-const config = { // providing parameters to the database, as well as identifying database
-    database: 'weekend-to-do-app',
-    host: 'localhost',
-    port: 5432,
-    max: 10,
-    idleTimeoutMillis: 30000
-};
+let config = {}
 
+if (process.env.DATABASE_URL) {
+    // Running remote (heroku)
+    config = { // providing parameters to the database, as well as identifying database
+        connectionString: process.env.DATABASE_URL,
+        ssl: {rejectUnauthenticated: false},
+    };
+
+} else {
+    // Running locally
+    config = { // providing parameters to the database, as well as identifying database
+        database: 'weekend-to-do-app',
+        host: 'localhost',
+        port: 5432,
+        max: 10,
+        idleTimeoutMillis: 30000
+    };
+}
+
+// Create the pool with the proper config
 const pool = new pg.Pool(config);
 
 pool.on("connect", () => {
